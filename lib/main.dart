c69330221edd09624abe20aa401ab3438fe101e2
mohammed <ibn.alsnbany@gmail.com>
2025-06-30 19:20:33 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.indigo,
+      appBar: AppBar(
+        title: Text('تطابق الصورة'),
+        backgroundColor: Colors.indigo[800],
+
+      ),
+      body: ImagePage(),
+    ),
+  ));
+}
+
+
+class ImagePage extends StatelessWidget {
+  const ImagePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
