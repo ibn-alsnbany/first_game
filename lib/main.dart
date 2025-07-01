2c1eed5fb83c1b1fb83cc620f93deab9e4e0deff
mohammed <ibn.alsnbany@gmail.com>
2025-07-01 23:59:22 +0300

    use void and set state

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -14,15 +15,31 @@ void main() {
   ));
 }
 
-
-class ImagePage extends StatelessWidget {
-  const ImagePage({super.key});
+class ImagePage extends StatefulWidget {
 
   @override
+  State<ImagePage> createState() => _ImagePageState();
+}
+
+class _ImagePageState extends State<ImagePage> {
+  
+    int LeftImageNumber = 1;
+    int RightImageNumber = 2;
+
+    void ChangImage() {
+      LeftImageNumber = Random().nextInt(8) +1;
+      RightImageNumber = Random().nextInt(8) +1;  
+    }
+  @override
   Widget build(BuildContext context) {
+    
+
     return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Text('حاول مرة اخرى',
+        Text(
+          LeftImageNumber == RightImageNumber ? 'مبروك لقد نجحت ' :
+          'حاول مرة اخرى',
         style: TextStyle(
           fontSize: 42.0,
           color: Colors.white,
@@ -33,15 +50,34 @@ class ImagePage extends StatelessWidget {
           children: [
             Expanded(
               
-              child: Image.asset('images/image-1.png'),
+            
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    ChangImage();  
+                  });
+                  
+                },
+                child: Image.asset('images/image-$LeftImageNumber.png')
+                ),
             ),
             Expanded(
               
-              child: Image.asset('images/image-1.png'),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    ChangImage();  
+                  });
+                  
+                },
+                child: Image.asset('images/image-$RightImageNumber.png'),
+                ),
             )
           ],
         ),
       ],
     );
   }
+
 }
+
